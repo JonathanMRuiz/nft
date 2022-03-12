a32a543290dc6c802ab21773a90c99e80f0b1481
JonathanMRuiz <jonimrz@yahoo.com>
2022-03-12 04:58:05 -0300

    first

---
+++ migrations/2_deploy.js
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+const ERC721PresetMinterPauserAutoId= artifacts.require("ERC721PresetMinterPauserAutoId");
+
+module.exports = function(deployer){
+    deployer.deploy(ERC721PresetMinterPauserAutoId)
+}

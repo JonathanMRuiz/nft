e584305b2bc18b2733104bb41b63b88cc06817c3
JonathanMRuiz <jonimrz@yahoo.com>
2022-03-12 04:59:22 -0300

    first

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,5 +3,5 @@
 const ERC721PresetMinterPauserAutoId= artifacts.require("ERC721PresetMinterPauserAutoId");
 
 module.exports = function(deployer){
-    deployer.deploy(ERC721PresetMinterPauserAutoId)
+    deployer.deploy(ERC721PresetMinterPauserAutoId,"HELLO NFT", "HELLONFT", )
 }
